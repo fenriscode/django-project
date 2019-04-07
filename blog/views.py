fee2705517d07d23b0064f764ae4f732244f3c66
Alexey Alert <alexey.alert@gmail.com>
2019-04-07 17:48:37 +0200

    First, initial commit

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Post
+from django.contrib.auth.models import User
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
